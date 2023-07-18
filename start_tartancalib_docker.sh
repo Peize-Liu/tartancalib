cbe4ab2d5a8706ea35b7fb71166f2739390ead63
MortyL <mortyl0834@gmail.com>
2023-07-18 11:29:54 +0800

    [Fix][script]:Modify for tartancalib

--- start_tartancalib_docker.sh
+++ start_tartancalib_docker.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#you need to image the repo dir into container
 REPO_PATH=/home/khalil/workspace/tartancalib
 FOLDER=/media/khalil/ssd_data/data_set
 xhost +
@@ -6,3 +7,4 @@ docker run -it --rm -e "DISPLAY" -e "QT_X11_NO_MITSHM=1" \
     -v "/tmp/.X11-unix:/tmp/.X11-unix:rw" \
     -v "$REPO_PATH:/tartancalib_ws/src/tartancalib" \
     -v "$FOLDER:/data" tartancalib:omni /bin/bash source ./catkin_ws/devel/setup.bash
+ 
