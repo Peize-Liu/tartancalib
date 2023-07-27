babf35b332aa190393b8fbca8bcecafeb2506f4f
MortyL <mortyl0834@gmail.com>
2023-07-27 11:33:10 +0800

    [Feat][Tools]: docker start script

--- start_calibration.sh
+++ start_calibration.sh
@@ -6,8 +6,8 @@ rosrun kalibr tartan_calibrate   --bag $BAG_PATH\
             /oak_ffc_4p/image_CAM_B/compressed\
             /oak_ffc_4p/image_CAM_C/compressed\
             /oak_ffc_4p/image_CAM_D/compressed\
-  --min-init-corners-autocomplete 30\
-  --min-tag-size-autocomplete 2\
+  --min-init-corners-autocomplete 30 \
+  --min-tag-size-autocomplete 2 \
   --models omni-radtan omni-radtan omni-radtan omni-radtan\
   --save_dir /data/output \
-  --intrinsic-prarameters /tartancalib_ws/src/tartancalib/intrinsic_parameters_for_test.yaml\
+  --intrinsic-prarameters /catkin_ws/src/kalibr/intrinsic_parameters_for_test.yaml
--- start_tartancalib_docker.sh
+++ start_tartancalib_docker.sh
@@ -2,9 +2,9 @@
 #you need to image the repo dir into container
 REPO_PATH=/home/khalil/workspace/tartancalib
 FOLDER=/media/khalil/ssd_data/data_set
+IMAGE=omnitartancalib:latest
 xhost +
 docker run -it --rm -e "DISPLAY" -e "QT_X11_NO_MITSHM=1" \
     -v "/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-    -v "$REPO_PATH:/tartancalib_ws/src/tartancalib" \
-    -v "$FOLDER:/data" tartancalib:omni /bin/bash source ./catkin_ws/devel/setup.bash
- 
+    -v "$REPO_PATH:/catkin_ws/src/kalibr" \
+    -v "$FOLDER:/data" ${IMAGE} /bin/bash
